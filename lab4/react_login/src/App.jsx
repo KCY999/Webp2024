f7e0fa9af106da1abab8dcbf7020de5cfbf2c383
KCY999 <yykuo0924@gmail.com>
2024-05-01 15:42:18 +0800

    Lab4

---
+++ lab4/react_login/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './CGU_login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {Login()}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
